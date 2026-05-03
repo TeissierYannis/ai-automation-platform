9f4a43c07ea14e427e2c7984ec044d497b201ba1
TeissierYannis <yannis.teissier70@gmail.com>
2026-05-03 12:55:49 +0200

    feat: Setup auth

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from "@/generated/prisma"
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+};
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL }),
+    });
 
-if (process.env.NODE_ENV === "production") {
-    globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
 }
 
-export default prisma
+export default prisma;
